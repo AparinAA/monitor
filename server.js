52dcaa3ec4c919c0246bd6dc09544ab70726a392
AparinAA <alxaparin@gmail.com>
2022-06-01 17:23:32 +0300

    upd layout

--- server.js
+++ server.js
@@ -207,9 +207,9 @@ const requestListener = function (req, res) {
          
 };
 
-const server = https.createServer(options, requestListener);
+const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on https://${host}:${port}`);
+    console.log(`Server is running on http://${host}:${port}`);
 });
 
 /*
