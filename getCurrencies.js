ff1b1bc871cbc3c7c31586abd11e461c92c6f94f
AparinAA <alxaparin@gmail.com>
2022-11-17 18:26:32 +0300

    add

--- getCurrencies.js
+++ getCurrencies.js
@@ -71,7 +71,6 @@ function promiseTickersWithSpread(exchanges, tickersAll, nsscrySpread) {
     .then(response => {
 
         //info tickers of OKX
-        console.info(response[1])
         const tickersOKX = response
         .filter(item => +item.volCcy24h > 100000)
         .map(item => {
