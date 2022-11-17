ac91ca5a827500f8dfe6e797a06a404ad7535c16
AparinAA <alxaparin@gmail.com>
2022-11-17 19:12:16 +0300

    add

--- src/App.js
+++ src/App.js
@@ -504,7 +504,7 @@ class ScanerPlot extends React.Component {
         //axios.get(`http://localhost:8090/allspread`)
         axios.get(`http://195.133.1.56:8090/allspread`)
         .then( res => {
-            console.info("RES", res)
+            console.info(res);
             this.setState({
                 allTickets: res.data,
                 loading: false,
